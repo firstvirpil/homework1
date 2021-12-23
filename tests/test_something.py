b99b2ee1992e7dbef10cad1cae1f0a77e30d9d69
virpil <firstvirpil@gmail.com>
2021-12-23 18:10:54 +0700

    testcases

---
+++ tests/test_something.py
@@ -0,0 +1,10 @@
+# канал SolveMe первое видео
+import pytest
+
+def test_equal():
+    assert 1 == 1, "Number is not equal to expected"
+
+
+def test_not_equal():
+    assert 1 != 2, "Number is equal"
+
